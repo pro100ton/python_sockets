ad95b48fbf7cb8053fb293761cc7b8ca3cb676f8
aoshalimov <aoshalimov@esc-center.ru>
2022-08-25 08:50:11 +0300

    Refactor some code

--- test.py
+++
@@ -1,9 +0,0 @@
-from main import broadcast
-import socket
-
-def socket_callbac():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.listen()
-
-if __name__ == '__main__':
-    socket_callbac()
